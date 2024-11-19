3ff6d504c81b7c35fa75cbfc01873c4c5585f3cd
Chirag-varu <chiragvaru03@gmail.com>
2024-11-19 21:44:59 +0530

    initializing

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome To Ultopia!</h1>
+      <h2>currently under developmenmt ...</h2>
+    </>
+  )
+}
+
+export default App
