95318fefee41ad28affb9932f838f530d000eede
Chirag-varu <chiragvaru03@gmail.com>
2024-11-21 21:42:02 +0530

    Docs and Components sample page added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
         <div className="mb-40">
             <Navbar />
         </div>
-      <p className="text-lg mt-4">We are here to make your experience better.</p>
+      <h1 className="text-lg mt-4">We are here to make your experience better.</h1>
     </div>
   );
 }
--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -1,12 +1,12 @@
 import { Navbar } from "../components/navbar";
 
-export default function Contact() {
+export default function Docs() {
   return (
     <div className="flex flex-col">
         <div className="mb-40">
             <Navbar />
         </div>
-      <h1 className="text-lg mt-4">Feel free to reach out at contact@example.com.</h1>
+      <h1 className="text-lg mt-4">Docs page.</h1>
     </div>
   );
 }
